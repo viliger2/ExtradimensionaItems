a95e7f83a0c29757789e7c8bd988792c23ee2b4f
viliger <wowlocale@gmail.com>
2023-01-29 19:08:38 +0300

    Implemented SkullOfDoom effect. (no, not again, I forgot to push the module)

---
+++ RoR2_ItemsMod/Modules/Effects/GroundFires.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace ExtradimensionalItems.Modules.Effects
+{
+
+    public class GroundFires : MonoBehaviour
+    {
+        const float timeBetweenSpawns = 0.15f;
+        private float timer;
+
+        public GameObject fire;
+
+        void Update()
+        {
+            if(timer <= 0)
+            {
+                Instantiate(fire, transform.position, Quaternion.identity);
+                timer = timeBetweenSpawns + UnityEngine.Random.Range(0f, 0.1f);
+            } else
+            {
+                timer -= Time.deltaTime;
+            }
+        }
+    }
+
+}
