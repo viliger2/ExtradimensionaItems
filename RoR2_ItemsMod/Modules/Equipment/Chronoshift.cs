a6f8f898b7d9787c1d0d7dc2fbfe983dee626fc2
viliger <wowlocale@gmail.com>
2023-01-29 20:58:02 +0300

    "implemented" proper log book display (as in class works and follows the points I've given it, now need to readjust the model) for Chronoshift Re-positioned item display for Chronoshift

--- RoR2_ItemsMod/Modules/Equipment/Chronoshift.cs
+++ RoR2_ItemsMod/Modules/Equipment/Chronoshift.cs
@@ -445,9 +445,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(-0.00088F, 0.10924F, -0.06687F),
-                    localAngles = new Vector3(59.57273F, 151.3646F, 327.5803F),
-                    localScale = new Vector3(0.67373F, 0.67373F, 0.67373F)                }
+                    localPos = new Vector3(0.01868F, 0.15171F, -0.10387F),
+                    localAngles = new Vector3(45.49194F, 183.5922F, 345.4405F),
+                    localScale = new Vector3(0.51963F, 0.51963F, 0.51963F)               }
             });
             rules.Add("mdlHuntress", new RoR2.ItemDisplayRule[]
             {
@@ -456,9 +456,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(0.0288F, 0.15945F, -0.01174F),
-                    localAngles = new Vector3(60.44611F, 149.7126F, 14.5503F),
-                    localScale = new Vector3(0.53117F, 0.53117F, 0.53117F)
+                    localPos = new Vector3(0.01835F, 0.08114F, 0.00864F),
+                    localAngles = new Vector3(31.32549F, 197.5746F, 36.63119F),
+                    localScale = new Vector3(0.48011F, 0.48011F, 0.48011F)
                 }
             });
             rules.Add("mdlToolbot", new RoR2.ItemDisplayRule[]
@@ -468,9 +468,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(0.05995F, 0.80019F, -1.16F),
-                    localAngles = new Vector3(52.71721F, 186.379F, 9.95464F),
-                    localScale = new Vector3(3.51943F, 3.51943F, 3.51943F)
+                    localPos = new Vector3(-0.16778F, 0.77172F, -1.07452F),
+                    localAngles = new Vector3(33.70054F, 189.291F, 358.7404F),
+                    localScale = new Vector3(3.03086F, 3.03086F, 3.03086F)
                 }
             });
             rules.Add("mdlEngi", new RoR2.ItemDisplayRule[]
@@ -480,9 +480,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(-0.00007F, 0.12585F, -0.18822F),
-                    localAngles = new Vector3(49.57471F, 172.3253F, 1.44653F),
-                    localScale = new Vector3(0.61499F, 0.61499F, 0.61499F)
+                    localPos = new Vector3(0.00006F, 0.14264F, -0.20751F),
+                    localAngles = new Vector3(31.79489F, 185.1696F, 355.3399F),
+                    localScale = new Vector3(0.48752F, 0.48752F, 0.48752F)
                 }
             });
             rules.Add("mdlMage", new RoR2.ItemDisplayRule[]
@@ -492,9 +492,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(-0.00138F, 0.03181F, -0.23878F),
-                    localAngles = new Vector3(34.9938F, 172.7735F, 1.38432F),
-                    localScale = new Vector3(0.33224F, 0.33224F, 0.33224F)
+                    localPos = new Vector3(0.01074F, 0.0313F, -0.2331F),
+                    localAngles = new Vector3(23.68356F, 193.0841F, 358.9043F),
+                    localScale = new Vector3(0.29518F, 0.29518F, 0.29518F)
                 }
             });
             rules.Add("mdlMerc", new RoR2.ItemDisplayRule[]
@@ -504,9 +504,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(0.00466F, 0.15284F, -0.16268F),
-                    localAngles = new Vector3(298.223F, 185.8875F, 178.754F),
-                    localScale = new Vector3(0.47598F, 0.47598F, 0.47598F)
+                    localPos = new Vector3(0.01603F, 0.15346F, -0.14982F),
+                    localAngles = new Vector3(311.9305F, 180.8366F, 179.298F),
+                    localScale = new Vector3(0.53588F, 0.53588F, 0.53588F)
                 }
             });
             rules.Add("mdlTreebot", new RoR2.ItemDisplayRule[]
@@ -516,8 +516,8 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "PlatformBase",
-                    localPos = new Vector3(-0.00033F, 0.04076F, -0.23915F),
-                    localAngles = new Vector3(43.94766F, 181.4132F, 0.98089F),
+                    localPos = new Vector3(-0.00033F, 0.04071F, -0.25942F),
+                    localAngles = new Vector3(31.31434F, 190.4047F, 0.61905F),
                     localScale = new Vector3(1.48928F, 1.48928F, 1.48928F)
                 }
             });
@@ -528,9 +528,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(0.017F, -0.05289F, -0.2494F),
-                    localAngles = new Vector3(40.70776F, 178.8556F, 353.7026F),
-                    localScale = new Vector3(0.61055F, 0.61055F, 0.61055F)
+                    localPos = new Vector3(0.0082F, -0.03117F, -0.26995F),
+                    localAngles = new Vector3(28.29659F, 186.0077F, 352.9836F),
+                    localScale = new Vector3(0.50544F, 0.50544F, 0.50544F)
                 }
             });
             rules.Add("mdlCroco", new RoR2.ItemDisplayRule[]
@@ -540,9 +540,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(-0.25128F, 0.5368F, 2.74673F),
-                    localAngles = new Vector3(59.82613F, 68.69579F, 76.82799F),
-                    localScale = new Vector3(6.09834F, 6.09834F, 6.09834F)
+                    localPos = new Vector3(-0.24748F, 0.28157F, 2.99287F),
+                    localAngles = new Vector3(45.40444F, 27.58954F, 32.31604F),
+                    localScale = new Vector3(5.1585F, 5.1585F, 5.1585F)
                 }
             });
             rules.Add("mdlCaptain", new RoR2.ItemDisplayRule[]
@@ -552,8 +552,8 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(-0.01424F, 0.16052F, -0.08735F),
-                    localAngles = new Vector3(305.6797F, 174.4509F, 187.9807F),
+                    localPos = new Vector3(-0.00684F, 0.16017F, -0.07591F),
+                    localAngles = new Vector3(332.7996F, 187.619F, 166.1224F),
                     localScale = new Vector3(0.67517F, 0.67517F, 0.67517F)
                 }
             });
@@ -564,23 +564,23 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(0.01333F, 0.17094F, -0.07179F),
-                    localAngles = new Vector3(35.16169F, 213.0192F, 44.44061F),
+                    localPos = new Vector3(-0.0091F, 0.18334F, -0.06412F),
+                    localAngles = new Vector3(8.04993F, 202.6387F, 37.24955F),
                     localScale = new Vector3(0.4842F, 0.4842F, 0.4842F)
                 }
             });
-            rules.Add("mdlMiner", new RoR2.ItemDisplayRule[]
-            {
-                new RoR2.ItemDisplayRule
-                {
-                    ruleType = ItemDisplayRuleType.ParentedPrefab,
-                    followerPrefab = ItemBodyModelPrefab,
-                    childName = "Chest",
-                    localPos = new Vector3(0F, 0.00339F, -0.00108F),
-                    localAngles = new Vector3(317.3549F, 221.5528F, 123.0072F),
-                    localScale = new Vector3(0.00557F, 0.00557F, 0.00557F)
-                }
-            });
+            //rules.Add("mdlMiner", new RoR2.ItemDisplayRule[]
+            //{
+            //    new RoR2.ItemDisplayRule
+            //    {
+            //        ruleType = ItemDisplayRuleType.ParentedPrefab,
+            //        followerPrefab = ItemBodyModelPrefab,
+            //        childName = "Chest",
+            //        localPos = new Vector3(0F, 0.00339F, -0.00108F),
+            //        localAngles = new Vector3(317.3549F, 221.5528F, 123.0072F),
+            //        localScale = new Vector3(0.00557F, 0.00557F, 0.00557F)
+            //    }
+            //});
             rules.Add("mdlHeretic", new RoR2.ItemDisplayRule[]
             {
                 new RoR2.ItemDisplayRule
@@ -589,7 +589,7 @@ namespace ExtradimensionalItems.Modules.Equipment
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
                     localPos = new Vector3(0.06566F, -0.22289F, -0.10183F),
-                    localAngles = new Vector3(23.48283F, 50.56009F, 233.6516F),
+                    localAngles = new Vector3(42.46209F, 59.69307F, 213.2425F),
                     localScale = new Vector3(1F, 1F, 1F)
                 }
             });
@@ -601,7 +601,7 @@ namespace ExtradimensionalItems.Modules.Equipment
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Backpack",
                     localPos = new Vector3(-0.02928F, -0.24107F, 0.00183F),
-                    localAngles = new Vector3(345.5057F, 132.0578F, 251.016F),
+                    localAngles = new Vector3(350.0237F, 150.1604F, 225.3768F),
                     localScale = new Vector3(0.56718F, 0.56718F, 0.56718F)
                 }
             });
@@ -612,9 +612,9 @@ namespace ExtradimensionalItems.Modules.Equipment
                     ruleType = ItemDisplayRuleType.ParentedPrefab,
                     followerPrefab = ItemBodyModelPrefab,
                     childName = "Chest",
-                    localPos = new Vector3(-0.00399F, 0.05458F, -0.06697F),
-                    localAngles = new Vector3(350.4411F, 203.1579F, 158.2204F),
-                    localScale = new Vector3(0.60104F, 0.60104F, 0.60104F)
+                    localPos = new Vector3(0.02187F, 0.06523F, -0.0598F),
+                    localAngles = new Vector3(350.3206F, 192.9503F, 149.9679F),
+                    localScale = new Vector3(0.60224F, 0.60224F, 0.60224F)
                 }
             });
             // EXAMPLE
@@ -642,6 +642,7 @@ namespace ExtradimensionalItems.Modules.Equipment
         public override void Init(ConfigFile config)
         {
             LoadAssetBundle();
+            SetLogbookCameraPosition();
             LoadSoundBank();
             CreateConfig(config);
             CreateEquipment(ref Content.Equipment.Chronoshift);
@@ -653,6 +654,18 @@ namespace ExtradimensionalItems.Modules.Equipment
             base.LoadSoundBank();
             Utils.RegisterNetworkSound("EI_Chronoshift_Start");
             Utils.RegisterNetworkSound("EI_Chronoshift_End");
+        }
+
+        private void SetLogbookCameraPosition()
+        {
+            var modelParameters = EquipmentModel.AddComponent<ModelPanelParameters>();
+
+            modelParameters.focusPointTransform = EquipmentModel.transform.Find("FocusPoint");
+            modelParameters.cameraPositionTransform = EquipmentModel.transform.Find("CameraPosition");
+            modelParameters.modelRotation = new Quaternion(21.43f, -10.32f, 0f, 1f);
+
+            modelParameters.minDistance = 1;
+            modelParameters.maxDistance = 3;
         }
 
         protected override void Hooks()
